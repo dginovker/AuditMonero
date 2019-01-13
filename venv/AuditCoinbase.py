14f2e3e0c2cb078a740e49f09ccfc57af1e3bfd2
dginovker <dcress01@mail.uoguelph.ca>
2019-01-13 14:07:54 -0500

    Added a verbose mode, added warnings, and added CSV output for emissions

--- venv/AuditCoinbase.py
+++ venv/AuditCoinbase.py
@@ -5,12 +5,41 @@ import requests
 
 START = 0
 END = 1742081
+#END = 15 #For testing purposes
 
-previousAvgOf10 = 0
+VERBOSE = True
+SCARY_MULTIPLIER = 2
+
 sum = 0
 
 url = "http://127.0.0.1:18081/json_rpc"
 headers = {'Content-Type': 'application/json'}
+
+previous10 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+def scaryHigh(emission):
+    emission = emission/10**13
+    if (emission > SCARY_MULTIPLIER*avgOf10()):
+        return True;
+    return False;
+
+
+def updatePrevious10(emission):
+    emission = emission/10**13
+    for x in range(9):
+        previous10[x] = previous10[x+1]
+    previous10[9] = emission
+
+
+def avgOf10():
+    total = 0
+    for x in previous10:
+        total += x
+
+    return total/10
+
+f = open("CoinbaseMints.csv", "w")
+f.write("Block number,Coinbase emission,Total fees\n")
 
 for i in range(END):
     payload = {
@@ -26,7 +55,19 @@ for i in range(END):
     response = requests.post(
         url, data=json.dumps(payload), headers=headers).json()
 
-
     sum += response["result"]["emission_amount"]
 
-print("Total Monero minted from block " + str(START) + " to " + str(END) + " was " + str(round(sum/10**13, 2)) + " XMR")
+    updatePrevious10(response["result"]["emission_amount"])
+
+    if scaryHigh(response["result"]["emission_amount"]):
+        print("WARN: Block " + str(i) + " has abnormally high reward of " + str(round(response["result"]["emission_amount"]/10**13, 2)) + " with fee total of " + str(response["result"]["fee_amount"]) + "(previous avg. was " + str(avgOf10()) + ")\n\t" + str(response) + "\n")
+
+    if response["result"]["status"] != 'OK':
+        print("WARN: Block " + str(i) + " did not give the OK!\n\t" + str(response) + "\n")
+
+    if i%10000 == 0 and VERBOSE:
+        print("Currently finished parsing block " + str(i) + ". Total emission is currently " + str(round(sum/10**13, 2)) + " XMR")
+
+    f.write(str(i) + "," + str(response["result"]["emission_amount"]) + "," + str(response["result"]["fee_amount"]) + "\n")
+
+print("\n\nTotal Monero minted from block " + str(START) + " to " + str(END) + " was " + str(round(sum/10**13, 2)) + " XMR")
